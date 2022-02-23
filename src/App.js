375d1de9e63d60441ab7d3768cce823c2e142cc0
leeheeae <chromeee@daum.net>
2022-02-23 14:00:31 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import Button from './Button';
 import styled from './App.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  //useState
   const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter((prev) => prev + 1);
+  const [keyword, setKeyword] = useState('');
 
-  console.log('call API');
+  //func
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  //useEffect
+  useEffect(() => {
+    console.log('한번만 실행');
+  }, []);
+  useEffect(() => {
+    console.log('counter');
+  }, [counter]);
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 5) {
+      console.log('search for', keyword);
+    }
+  }, [keyword]);
+
+  //start
   return (
     <div>
+      <input
+        onChange={onChange}
+        value={keyword}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styled.title}>{counter}</h1>
       <Button text={'Continue'} onClick={onClick} />
     </div>

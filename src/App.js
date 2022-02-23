df2d418a6212de8fed1c061c6ee0beb5ca98bc35
leeheeae <chromeee@daum.net>
2022-02-23 16:05:50 +0900

    코인변환기

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [btc, setBtc] = useState(0);
+  const [conversion, setConversion] = useState(undefined);
+  const [selectCoin, setSelectCoin] = useState('');
+
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  const onSelect = (event) => setSelectCoin(event.target.value);
+  const coinChange = (event) => setBtc(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === '') return;
-    setToDos((prev) => [toDo, ...prev]);
-    setToDo('');
+    setConversion(Math.floor(btc / selectCoin));
   };
-  useEffect(() => {
-    console.log(toDos);
-  }, [toDos]);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
+      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
+
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onSelect}>
+          <option>코인을 선택해주세요.</option>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
       <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <div>
+        <h2>코인 전환</h2>
+        <form onSubmit={onSubmit}>
+          <input
+            onChange={coinChange}
+            value={btc}
+            type="number"
+            placeholder="보유 중인 BTC를 입력해주세요."
+          />
+          <button>전환</button>
+        </form>
+        <div>
+          전환 된 코인 수 : {conversion === undefined ? null : conversion}
+        </div>
+      </div>
     </div>
   );
 }

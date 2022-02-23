fa5af3c80f926424d1207b96c3b81acedb936e47
leeheeae <chromeee@daum.net>
2022-02-23 14:20:56 +0900

    clean Fn

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-import Button from './Button';
-import styled from './App.module.css';
 import { useState, useEffect } from 'react';
+
+function Hello() {
+  //func
+  function byeFn() {
+    console.log('destoryed : (');
+  }
+  function hiFn() {
+    console.log('i`m here!');
+    return byeFn;
+  }
+
+  //useEffect
+  useEffect(hiFn, []);
+
+  return <h1>Hello</h1>;
+}
 
 function App() {
   //useState
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState('');
-
+  const [showing, setShowing] = useState(false);
   //func
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  const onClick = () => setShowing((prev) => !prev);
 
   //useEffect
-  useEffect(() => {
-    console.log('한번만 실행');
-  }, []);
-  useEffect(() => {
-    console.log('counter');
-  }, [counter]);
-  useEffect(() => {
-    if (keyword !== '' && keyword.length > 5) {
-      console.log('search for', keyword);
-    }
-  }, [keyword]);
 
   //start
   return (
     <div>
-      <input
-        onChange={onChange}
-        value={keyword}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styled.title}>{counter}</h1>
-      <Button text={'Continue'} onClick={onClick} />
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
